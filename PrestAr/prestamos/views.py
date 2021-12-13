c6a5f2828e82dd99f727cfcc5f55294303ff6547
Augusto <autusgo@outlook.com>
2021-12-12 21:28:49 -0300

    arreglada redirección después de eliminar simulacion

--- PrestAr/prestamos/views.py
+++ PrestAr/prestamos/views.py
@@ -54,7 +54,10 @@ def simulacion_edit(request, pk):
 def simulacion_remove(request, pk):
     simulacion = get_object_or_404(Simulacion, pk=pk)
     simulacion.delete()
-    return render(request, 'simulador/simulaciones_list.html', {'filter': simulacion})
+
+    simulaciones = SimulacionFilter(
+        request.GET, queryset=Simulacion.objects.all())
+    return render(request, 'simulador/simulaciones_list.html', {'filter': simulaciones})
 
 
 def sobre_conami(request):
