e37fa35fc74337e7035d484d282f30eda23ff4ec
Augusto <autusgo@outlook.com>
2021-12-21 00:34:39 -0300

    agregado el filtro de fechas

--- PrestAr/prestamos/filters.py
+++ PrestAr/prestamos/filters.py
@@ -1,13 +1,22 @@
+from django.db.models.fields import DateField
 import django_filters
 from .models import Simulacion
-from django_filters import FilterSet, CharFilter
+from django_filters import FilterSet, CharFilter, DateFilter, NumericRangeFilter
+from django import forms
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 
 class SimulacionFilter(django_filters.FilterSet):
     nom_ape = CharFilter(field_name="nom_ape", lookup_expr='icontains')
-    created_date = CharFilter(
-        field_name="created_date", lookup_expr='icontains')
+    start_date = DateFilter(field_name="created_date",
+                            lookup_expr='gte', widget=DateInput)
+    end_date = DateFilter(field_name="created_date",
+                          lookup_expr='lte', widget=DateInput)
+    # monto_desde = NumericRangeFilter(field_name="monto")
 
     class Meta:
         model = Simulacion
-        fields = ['nom_ape', 'created_date', 'email']
+        fields = '__all__'
